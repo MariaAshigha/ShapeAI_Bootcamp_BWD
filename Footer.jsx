c9d81cd45c5c2c3cf89a072e03f2e31eefbb3237
MariaAshigha <75783317+MariaAshigha@users.noreply.github.com>
2021-06-07 00:46:52 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  var curryear = new Date().getFullYear();
+  return (
+  <Footer>
+    <p>Copyright @ {curryear}</p>
+  </Footer>
+  );
+}
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return(
+  <div className="note">
+    <h1>Javascript and React.js</h1>
+    <p>
+      Hi, im Maria Ashigha, I learned Java Script and React from ShapeAI 7-
+      day Free Bootcamp. It was Awesome.
+    </p>
+  </div>
+  );
+}
+export default Note;
